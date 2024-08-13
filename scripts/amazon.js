49293b2f24093424296d1721dc4d8175e1da113c
Soghra Mozafari <Atifahzafari@gmail.com>
2024-08-13 09:04:55 -0400

    fix price formating and radio selectors

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -4,6 +4,7 @@ import {cart, addToCart} from '../data/cart.js';
 //order of the scripts loaded does not matter. order does not matter
 
 import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
 
@@ -29,7 +30,7 @@ products.forEach((product)=> {
           </div>
 
           <div class="product-price">
-            ${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
