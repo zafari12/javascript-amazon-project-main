978884a636eb3e6330a0cbf1b9fac33ba0989e19
Soghra Mozafari <Atifahzafari@gmail.com>
2024-08-26 10:17:27 -0400

    convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -112,6 +112,35 @@ object3.method();
 
 export let products = [];
 
+export function loadProductsFetch (){
+  //creates a promise
+  //save the response in the parameter
+const promise = fetch(
+  'https://supersimplebackend.dev/products'
+).then((response)=>{
+//to get data from the response. data attached to response
+   return response.json();
+  }).then((productsData)=> {
+    products = productsData.map((productDetails)=> {
+      if (productDetails.type === 'clothing') {
+        return new Clothing (productDetails);
+      }
+       return new Product(productDetails);
+    });
+
+    console.log('load products');
+    
+  });
+
+  return promise;
+}
+
+/*
+loadProductsFetch().then(()=> {
+  console.log('next step');
+});
+
+
 
 export function loadProducts (fun) {
 
@@ -138,7 +167,7 @@ export function loadProducts (fun) {
   xhr.send();
 }
 
-
+*/
 /*
 export const products = [
   {
