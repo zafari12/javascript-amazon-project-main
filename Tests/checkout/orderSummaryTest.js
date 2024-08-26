2f32b4d5723eec4056c7a1ff9a62600b2fcdbb53
Soghra Mozafari <Atifahzafari@gmail.com>
2024-08-26 09:10:06 -0400

    load products in beforeAll hook

--- Tests/checkout/orderSummaryTest.js
+++ Tests/checkout/orderSummaryTest.js
@@ -1,10 +1,17 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js"; 
 import {loadFromStorage, cart} from "../../data/cart.js";
+import {loadProducts} from '../../data/products.js';
 
 describe('test suite: renderOrderSummary', ()=> {
 const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
 const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
+beforeAll((done)=> {
+  loadProducts(()=> {
+    done();
+  });
+  
+});
   beforeEach(()=> {
     spyOn(localStorage, 'setItem');
     document.querySelector('.js-test-container').innerHTML = `
@@ -36,9 +43,9 @@ it('displays the cart', ()=> {
       document.querySelector(`.js-product-quantity-${productId1}`).innerText
     ).toContain('Quantity: 2');
 
-   /* expect(
+    expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
-    ).toContain('Quantity: 1');*/
+    ).toContain('Quantity: 1');
 
     document.querySelector('.js-test-container').innerHTML = ``; 
    });
