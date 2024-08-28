21316af888f1789d97a9231da600ca343c5325f9
Soghra Mozafari <Atifahzafari@gmail.com>
2024-08-28 08:38:02 -0400

    create order using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import {cart} from '../../data/cart.js';
 import {getProduct} from '../../data/products.js';
 import {getDeliveryOption} from '../../data/deliveryOptions.js';
 import {formatCurrency} from '../utils/money.js';
+import {addOrder} from '../../data/order.js';
 
 export function renderPaymentSummary(){
 let productPriceCents = 0; 
@@ -57,11 +58,35 @@ const paymentSummaryHTML = `
             ${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
     `;
 
   document.querySelector('.js-payment-summary')
    .innerHTML = paymentSummaryHTML;
+
+   document.querySelector('.js-place-order')
+   .addEventListener('click', async ()=> {
+     try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+
+      const order = await response.json();
+      addOrder(order);
+     } catch (error) {
+        console.log('unexpected error/ please try again later.');
+     }
+     //control the url at the top of the browser
+     //.href gives us the url at the top
+     //order.html is a file path
+     window.location.href = 'orders.html';
+   });
 }
